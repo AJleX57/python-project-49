110c392536ce2d981f654a51fbbce0a338deed28
Александр Твинков <s.alex06.a@gmail.com>
2025-11-19 23:20:50 +0300

    Create gcd game

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+from brain_games.engine import run_game
+from brain_games.games.gcd import generate_gcd_question
+
+
+def main():
+    description = "Find the greatest common divisor of given numbers."
+    run_game(description, generate_gcd_question)
+    
+    
+if __name__ == '__main__':
+    main()
