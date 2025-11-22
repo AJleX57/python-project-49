b7027b089b3d7281834e07b2813b3cf1add89196
Александр Твинков <s.alex06.a@gmail.com>
2025-11-22 23:12:27 +0300

    Add brain-progression game

---
+++ brain_games/games/progression.py
@@ -0,0 +1,25 @@
+import random
+
+
+def generate_arithmetic_progression(start, step, length):
+    """Генерирует арифметическую прогрессию"""
+    return [start + i * step for i in range(length)]
+
+
+def generate_progression_question():
+    """Генерация вопроса и ответа для прогрессии"""
+    start = random.randint(1, 20)
+    step = random.randint(1, 10)
+    length = random.randint(5, 10)
+    
+    progression = generate_arithmetic_progression(start, step, length)
+    hidden_index = random.randint(0, length - 1)
+    
+    progression_with_hidden = progression.copy()
+    correct_answer = str(progression_with_hidden[hidden_index])
+    progression_with_hidden[hidden_index] = ".."
+    
+    question = ' '.join(map(str, progression_with_hidden))
+    
+    return question, correct_answer    
+    
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,10 @@
+from brain_games.engine import run_game
+from brain_games.games.progression import generate_progression_question
+
+def main():
+    description = 'What is the result of the expression?'
+    run_game(description, generate_progression_question)
+
+
+if __name__ == '__main__':
+    main()
